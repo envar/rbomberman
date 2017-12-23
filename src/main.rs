49bbff9c3cd8405743e043708dabe7bdcfa8f1ad
Michal Nicpon <nicpon.michal@gmail.com>
2017-12-23 10:46:03 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+extern crate cgmath;
+
+#[macro_use]
+extern crate nom;
 
 #[macro_use]
 extern crate gfx;
@@ -13,28 +17,47 @@ use gfx::Device;
 use glutin::GlContext;
 use gfx::traits::FactoryExt;
 
+mod obj;
+
 pub type ColorFormat = gfx::format::Rgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
 
 gfx_defines! {
     vertex Vertex {
-        pos: [f32; 2] = "a_Pos", // position coordinates
-        // col: [f32; 4] = "a_Col", // color
-        uv: [f32; 2] = "a_Uv", // texture coordinates
+        pos: [f32; 2] = "a_Pos",
+        uv: [f32; 2] = "a_Uv",
+        // normal: [gfx::format::I8Norm; 4] = "a_Normal",
     }
 
     constant Locals {
         transform: [[f32; 4]; 4] = "u_Transform",
     }
 
+    constant Globals {
+        mx_vp: [[f32; 4]; 4] = "u_ViewProj",
+    }
+
+    constant WireframeParam {
+        width: f32 = "u_WireWidth",
+        color: [f32; 4] = "u_WireColor",
+    }
+
+    pipeline wireframe_pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        cb_locals: gfx::ConstantBuffer<Locals> = "u_Locals",
+        cb_globals: gfx::ConstantBuffer<Globals> = "u_Globals",
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
+    }
+
     pipeline pipe {
         vbuf: gfx::VertexBuffer<Vertex> = (),
         tex: gfx::TextureSampler<[f32; 4]> = "t_Texture",
-        locals: gfx::ConstantBuffer<Locals> = "Locals",
         transform: gfx::Global<[[f32; 4]; 4]> = "u_Transform",
         out: gfx::RenderTarget<ColorFormat> = "Target0",
     }
 }
+
+// const VIEWPORT: [i32; 4] = [0, 0, 600, 800];
 
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
@@ -61,10 +84,10 @@ const TEX_QUAD: [Vertex; 4] = [
 const TEX_INDICES: [u16; 6] = [0, 1, 2, 2, 3, 0];
 
 const LOCALS: Locals = Locals {
-        transform: [[0.5, 0.0, 0.0, 0.0],
+        transform: [[1.0, 0.0, 0.0, 0.0],
                     [0.0, 1.0, 0.0, 0.0],
                     [0.0, 0.0, 1.0, 0.0],
-                    [0.0, 0.0, 0.0, 1.0]]
+                    [0.0, 0.0, 0.0, 1.0]],
 };
 
 pub fn main() {
@@ -74,11 +97,11 @@ pub fn main() {
     // initialize window
     let builder = glutin::WindowBuilder::new()
         .with_title("rbomberman".to_string())
-        .with_dimensions(640, 480);
+        .with_dimensions(800, 600);
     let context = glutin::ContextBuilder::new()
         .with_vsync(true);
     let mut events_loop = glutin::EventsLoop::new();
-    let (window, mut device, mut factory, main_color, mut main_depth) =
+    let (window, mut device, mut factory, main_color, main_depth) =
         gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder, context, &events_loop);
 
     // create encoder
@@ -88,18 +111,31 @@ pub fn main() {
     let sampler = factory.create_sampler_linear();
     let texture = gfx_load_texture(&mut factory);
     let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&TEX_QUAD, &TEX_INDICES[..]);
-    let locals_buffer = factory.create_constant_buffer(1);
     let data = pipe::Data {
         vbuf: vertex_buffer,
         tex: (texture, sampler),
-        locals: locals_buffer,
         transform: LOCALS.transform,
+        // line_width: LOCALS.line_width,
+        // line_color: LOCALS.line_color,
         out: main_color,
     };
 
+    // load shaders
+    // let shader_set = factory.create_shader_set_geometry(
+    //     include_bytes!("shaders/wireframe_vert.glsl"),
+    //     include_bytes!("shaders/wireframe_geom.glsl"),
+    //     include_bytes!("shaders/wireframe_frag.glsl"),
+    //     ).unwrap();
+
+    // create pso
+    // let pso = factory.create_pipeline_state(
+    //     &shader_set, gfx::Primitive::TriangleList, gfx::state::Rasterizer::new_fill(),
+    //     pipe::new()
+    //     ).unwrap();
+
     let pso = factory.create_pipeline_simple(
-        include_bytes!("shaders/myshader_150.glslv"),
-        include_bytes!("shaders/myshader_150.glslf"),
+        include_bytes!("shaders/basic_vert.glsl"),
+        include_bytes!("shaders/basic_frag.glsl"),
         pipe::new()
         ).unwrap();
 
--- src/shaders/wireframe_frag.glsl
+++ src/shaders/wireframe_frag.glsl
@@ -1,9 +1,22 @@
 #version 150 core
 
-in vec4 g_Color;
+noperspective in vec3 g_Dist;
+
+out vec4 Target0;
+
+// The mesh line settings
+uniform float u_LineWidth;
+uniform vec4 u_LineColor;
+
+vec4 color = vec4(0.0, 0.5, 1.0, 1.0);
 
 void main() {
-    gl_FragColor = vec4(1.0, 1.0, 1.9
+	// find the smallest distance
+    float d = min(g_Dist.x, min(g_Dist.y, g_Dist.z));
 
-    Target0 = texture(t_Texture, v_Uv);
+    // Determine the mix factor with line color
+    float mixVal = smoothstep(u_LineWidth - 1, u_LineWidth + 1, d);
+
+    // Mix the surface color with the line color
+	Target0 = mix(u_LineColor, color, mixVal);
 }
